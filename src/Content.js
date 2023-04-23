5d522ca248174f57d0fa2ecdb2a349dcb69da44c
Nguyenbahoa-sacly <khoab2306@gmail.com>
2023-04-24 01:04:36 +0700

    commit

--- src/Content.js
+++ src/Content.js
@@ -11,87 +11,22 @@ import { useEffect,useState } from "react"
 //callback luon duojc goi sau khi component mounted
 
 // element cua button
-const tabs= ['posts','comments','albums']
+
 
 function Content(){
-    //state cua input
-    const [title,setTitle] = useState('')
-    // state cua API
-    const [posts, setPosts] = useState([])
-    // state button
-    const [type,setType] = useState('posts')
-
-    const [goToTop, setGoToTop] = useState(false)
-
-    useEffect( ()=>{
-        fetch(`https://jsonplaceholder.typicode.com/${type}`)
-        
-            .then(res => res.json())
-            .then(posts =>{
-                setPosts(posts)
-        })
-    },[type])
-
-    useEffect( ()=>{
-        const hanldeScroll = ()=>{
-
-            if(window.scrollY >= 200){
-                setGoToTop(true)
-            }else{
-                setGoToTop(false)
-            }
-        }
-
-        //them su kien cuon o pham vi window
-        window.addEventListener('scroll',hanldeScroll)
-
-        //xoa bo su kien cuon o pham vi window
-        return ()=>{
-
-            window.removeEventListener('scroll',hanldeScroll)
-        }
+    const [countdown, setCountdow] = useState(180)
+    console.log(countdown);
+    useEffect(()=>{
+        const time = setInterval (()=>{
+                setCountdow(prevState => prevState - 1)
+            },1000)
+            
+            return()=>clearInterval(time)
     },[])
 
     return(
         <div>
-            {tabs.map(tab =>(
-                <button
-                    key ={tab}
-                    style={type === tab ? {
-                        color:'#fff',
-                        backgroundColor:"#333"
-                    }:{}}
-                    onClick={e=>setType(tab)}
-                >
-                    {tab}
-                </button>
-            ))}
-
-            <input 
-            value={title}
-            onChange={e => setTitle(e.target.value)}
-            />
-
-            <ul>
-                {posts.map(post =>(
-                    <li key = {post.id}>{post.title || post.name}</li>
-                ))}
-            </ul>
-
-            {goToTop && (
-                <button
-                    style={
-                        {
-                            position:"fixed",
-                            right:20,
-                            bottom:20
-                        }
-                    }
-                >
-                    GO TO TOP
-                </button>
-            )}
-            
+            <h1>{countdown}</h1>
         </div>
     )
 }
