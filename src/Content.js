d370e9d1035e51d90cd417a9e3354b3aa161109d
Nguyenbahoa-sacly <khoab2306@gmail.com>
2023-04-26 01:01:20 +0700

    commit

--- src/Content.js
+++ src/Content.js
@@ -1,4 +1,4 @@
-import { useEffect,useState } from "react"
+import { useEffect, useState, useLayoutEffect } from "react"
 
 //1. useEffect (callback)
 // - gọi callback mõi khi component re-render
@@ -13,67 +13,34 @@ import { useEffect,useState } from "react"
 //2. Cleanup funtion luon duoc goi truoc khi component unmounted
 //3. Cleanup funtion luon duoc goi truoc khi callback duoc goi ( trừ lần mounted)
 
-//dùng để list ra để chọn nội dung bài học
-const lessons=[
-    {
-        id:1,
-        name:'noi dung bai 1'
-    },
-    {
-        id:2,
-        name:'noi dung bai 2'
-    },
-    {
-        id:3,
-        name:'noi dung bai 3'
+/**useLayoutEffect
+ * 1 Cập nhật lại sate
+ * 2 Cập nhật lại DOM
+ * 3 gọi cleanup nếu deps thay đổi (sync)
+ * 4 gọi useLayoutEffect callback (sysnc)
+ * 5 Render lại UI
+*/
+function Content(){
+    const [count,setCount] = useState(0)
+
+    useLayoutEffect(()=>{
+        if(count > 3)
+            setCount(0)
+    },[count])
+
+    const hanldeClick = () =>{
+        setCount(count + 1 )
     }
-]
-function Content(){ 
 
-    const [lessonId, setLessonId] = useState(1)
 
-    useEffect(()=>{
-
-        //log ra comment của bài học
-        const hanldeComment = ({detail})=>{
-            console.log(detail);
-        }
-
-        //lây nội dùng fake comment từ file index.js
-        window.addEventListener(`lesson -${lessonId}`,hanldeComment)
-
-        return ()=>{
-        //Đóng nội dùng fake comment từ file index.js
-        window.removeEventListener(`lesson -${lessonId}`,hanldeComment)
-
-        }
-    },[lessonId])
-    
     return(
         <div>
-            <ul>
-                {
-                    //lay các bài học từ arr
-                lessons.map((lesson)=>(
-                    <li
-                        key={lesson.id} 
-                        style={
-                            {   
-                                /**nếu id của lessonid bằng lesson.id thi noi dung
-                                bai học mau do, con ko thì màu đen*/
-                                color:lessonId === lesson.id ?
-                                'red': '#333'
-                            }
-                        }
-                        /**click vao roi gan id lay duocj cho
-                        setLessonid treen leen cho lessonis*/
-                        onClick={()=> setLessonId(lesson.id)}
-                    >
-                        {lesson.name}
-                    </li>
-                ))
-                }
-            </ul>
+            <h1>{count}</h1>
+            <button
+            onClick={hanldeClick}
+            >
+                Run
+            </button>        
         </div>
     )
 }
