474a512b8c81d366bcf2977f17ac1a27b69185bd
Nguyenbahoa-sacly <khoab2306@gmail.com>
2023-04-30 21:59:03 +0700

    commit

--- src/Content.js
+++ src/Content.js
@@ -1,70 +1,51 @@
-import { useState,useMemo,useRef } from "react"
-// import Content2 from './Content2'
+import { useReducer } from "react"
+
 /**
- *  useMeno dung để tránh thực hiện lại 1 đoạn logic ko cần thiết
+ * useState
+ * 1. Init state: 0
+ * 2. Action: Up (state + 1), down (state - 1)
+
+ * useReducer 
+ * 1. Init state: 0
+ * 2. Action: Up (state + 1), down (state - 1)
+ * 3. Reducer
+ * 4. Dispatch
+ * 
  */
+//gia tri khoi tao
+const initState = 0
+//hang dong 
+const DOW_ACTION ='dow'
+const UP_ACTION = 'up'
 
-function Content(){
-
-    const [name,setName] = useState('')
-    const [price,setPrice] = useState('')
-    const [product,setProduct] =useState([])
-
-    const nameref = useRef()
-
-    const handleSubmit = () =>{
-        setProduct([...product,{
-            name,
-            price: +price
-        }])
-        
-        setName('')
-        setPrice('')
-        nameref.current.focus()
+const reducer = (state,action)=>{
+    //state nhan gia tri khoi tao
+    // action nhan hanh dong dow, up
+    switch(action){
+        //lua chon hanh dong
+        case UP_ACTION:
+            return state + 1
+            //gia tri khoi tao la gì thi return ra la loại đó
+        case DOW_ACTION:
+            return state - 1
+        default:
+            throw new Error('loi roi')
     }
+}
 
-    // tinh tong down hang
-    const total = useMemo(()=>{
-        const result= product.reduce((result,prod) =>{
-            console.log("Tính toán lại...")
-            return result + prod.price
-        },0)
-        return result
-    },[product])
 
-    // const total = product.reduce((result,prod)=>{
-    //     console.log("Tính toán lại...");
-    //     return result + prod.price
-    // },0)
-
+function Content(){ 
+    const [count , dispatch] = useReducer(reducer,initState)
+/**
+ * reducer nhan vao 2 tham so 
+ * 1. la ham
+ * 2. la gia tri khoi tao
+ */
     return(
         <div style={{padding:20}}>
-            <input 
-                ref={nameref}
-                value={name}
-                placeholder="Enter name..."
-                onChange={e =>setName(e.target.value) }
-            />
-            <br />
-            <input 
-                value={price}
-                placeholder="Enter price..."
-                onChange={e => setPrice(e.target.value)}
-            />
-            <br />
-            <button onClick={handleSubmit}>
-                Add
-            </button>
-            <br />
-            Total:{total}
-            <ul>{ product.map((product,index)=>{
-                    return(
-                        <li key={index}>
-                            {product.name} -{product.price}
-                        </li>
-                    )})
-                }
-            </ul>
+            <h2>{count}</h2>
+            <button onClick={()=> dispatch(UP_ACTION)}>Up</button>
+            <button onClick={()=> dispatch(DOW_ACTION)}>Down</button>
         </div>
     )
 }
