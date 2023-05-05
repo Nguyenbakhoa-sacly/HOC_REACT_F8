184b9102b6422d3685320aa89e68056a0017a863
Nguyenbahoa-sacly <khoab2306@gmail.com>
2023-05-05 23:54:22 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import Content from "./Todo/Content";
+import Content from "./Content";
+import './App.css';
+import { useContext } from "react";
+import { themeContext } from "./ThemeContext";
+// Context 
+// CompA => CompC
+
+
 
 function App() {
+  // nhan 1 object tu themeContext
+  const toggleValue= useContext(themeContext);
 
   return ( 
-    <div className="App" style={{padding: 20}}>
-      <Content />
-    </div>
-  );
+      <div>
+        <button onClick={toggleValue.toggleTheme}>Toggle theme</button>
+        <Content />
+      </div>
+    );
 }
 
 export default App;
--- src/Todo/contants.js
+++
@@ -1,5 +0,0 @@
-//cac hang
-// 2. action: hanh dong
-export const SET_JOB = 'set_job'//dung de set lai job:''
-export const ADD_JOB ='add_job' //button add
-export const DELETE_JOB = 'delete_job' //button delete
